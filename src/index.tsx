44c1ff0125ccea164bbe2c4a70730729fe9160e3
Maxmudov <qutbiddinmaxmudov@mail.ru>
2021-08-15 15:26:40 +0500

    add .env

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.scss'
 import App from './App'
-import { Provider } from 'react-redux'
 import { store } from './store/store'
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/gameApi.ts
+++ src/services/gameApi.ts
@@ -1,14 +1,13 @@
-import axios from "axios"
-import { MemberInterface } from "../types"
-
+import axios from 'axios'
+import { MemberInterface } from '../types'
 
 export const gameApi = {
-  async fetchRecords(): Promise<MemberInterface[]>{
-    const { data } = await axios.get('https://611758a730022f0017a05dda.mockapi.io/memory-game/members?sortBy=time')
+  async fetchRecords(): Promise<MemberInterface[]> {
+    const { data } = await axios.get(`https://${process.env.REACT_APP_MOCKAPI_ID}.mockapi.io/memory-game/members?sortBy=time`)
     return data
   },
-  async addNewRecord(record:MemberInterface): Promise<MemberInterface[]>{
-    const { data } = await axios.post('https://611758a730022f0017a05dda.mockapi.io/memory-game/members', record)
+  async addNewRecord(record: MemberInterface): Promise<MemberInterface[]> {
+    const { data } = await axios.post(`https://${process.env.REACT_APP_MOCKAPI_ID}.mockapi.io/memory-game/members`, record)
     return data
-  }
+  },
 }
